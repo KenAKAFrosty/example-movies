c631b0961c99536d22bdffb0d652e7f0ad59e3bd
FrostyyPaws <thefrostyypaws@gmail.com>
2024-03-10 20:02:28 -0500

    basic search scaffold, some other misc cleanup

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,33 +1,10 @@
-import { component$, useStylesScoped$ } from "@builder.io/qwik";
-import { routeLoader$, type DocumentHead } from "@builder.io/qwik-city";
+import {
+  component$,
+  useStylesScoped$
+} from "@builder.io/qwik";
+import { routeLoader$, type DocumentHead, Link } from "@builder.io/qwik-city";
 import { sql } from "kysely";
-import { GitHubLogo, QwikLogo } from "~/components/icons";
 import { getQueryBuilder } from "~/database/query_builder";
-
-export default component$(() => {
-  useStylesScoped$(`
-    main { 
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      min-height: 100vh;
-      min-height 100dvh;
-    }
-    div { 
-      flex-grow: 1;
-    }
-  `);
-
-  return (
-    <main>
-      <Header />
-      <div>
-        <Content />
-      </div>
-      <Footer />
-    </main>
-  );
-});
 
 export const useRandomMovies = routeLoader$(() => {
   return getQueryBuilder()
@@ -38,103 +15,40 @@ export const useRandomMovies = routeLoader$(() => {
     .limit(12)
     .execute();
 });
-const Content = component$(() => {
+
+export default component$(() => {
   useStylesScoped$(`
-      h1 { 
-          text-align: center;
-          margin-bottom: 12px;
-      }
-      ul { 
-        width: 380px;
-      }
-  `);
+  main { 
+    padding-top: 32px;
+  }
+  h1 { 
+      text-align: center;
+      margin-bottom: 12px;
+  }
+  ul { 
+    width: 380px;
+  }
+  h2 { 
+    font-weight: normal;
+    font-size: 18px;
+  }
+`);
+
 
   const randomMovies = useRandomMovies();
+
   return (
     <main>
-      <h1>Movies!</h1>
+
+      <h2>Here are a few random movies</h2>
       <ul>
         {randomMovies.value.map((row) => (
           <li key={row.title}>
-            <a href={`/movie/${row.id}`}>{row.title}</a>
+            <Link href={`/movie/${row.id}`}>{row.title}</Link>
           </li>
         ))}
       </ul>
     </main>
-  );
-});
-
-const Header = component$(() => {
-  useStylesScoped$(`
-    header { 
-      width: 100%;
-      display: flex;
-      justify-content: flex-end;
-      padding: 12px;
-    }
-    a svg { 
-      height: 1em;
-      width: 1em;
-      margin-bottom: -2px;
-    }
-  `);
-  return (
-    <header>
-      <a href="https://github.com/KenAKAFrosty/example-movies">
-        <GitHubLogo />
-        Code
-      </a>
-    </header>
-  );
-});
-
-const Footer = component$(() => {
-  useStylesScoped$(`
-    footer { 
-      padding: 8px;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      gap: 18px;
-      font-size: 12px;
-    }
-    div { 
-      display: flex;
-      align-items: center;
-    }
-    .qwik a { 
-      color: inherit;
-    }
-    .qwik a svg { 
-      height: 2em;
-      margin-bottom: -5px;
-      margin-right: -12px;
-    }
-    .qwik { 
-      font-size: 18px;
-    }
-    .others a { 
-      min-width: 180px;
-      text-align: center;
-    }
-  `);
-  return (
-    <footer>
-      <div class="qwik">
-        <p>Powered by </p>
-        <a href="https://qwik.dev">
-          <QwikLogo />
-        </a>
-      </div>
-      <div class="others">
-        <a href="https://remix-movies.pages.dev/">
-          Ryan's Original Remix Example
-        </a>
-        <a href="https://sveltekit-movies-demo.vercel.app/">
-          Rich's SvelteKit Example
-        </a>
-      </div>
-    </footer>
   );
 });
 
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,7 +1,19 @@
-import { component$, Slot, useStyles$ } from "@builder.io/qwik";
-import { type DocumentHead } from "@builder.io/qwik-city";
+import {
+  $,
+  component$,
+  Signal,
+  Slot,
+  sync$,
+  useOnWindow,
+  useSignal,
+  useStyles$,
+  useStylesScoped$,
+  useTask$,
+} from "@builder.io/qwik";
+import { Link, type DocumentHead, useLocation } from "@builder.io/qwik-city";
 import "@fontsource-variable/inter";
 import interFontDeclarationString from "@fontsource-variable/inter?inline";
+import { GitHubLogo, QwikLogo } from "~/components/icons";
 
 function extractUrlsFromFontsourceString(fontsourceString: string) {
   const extractUrlsFromFontsourceStringRegex = /url\(([^)]+)\)/g;
@@ -52,7 +64,7 @@ export default component$(() => {
     select {
         font: inherit;
     }
-    
+
     span,
     p,
     h1,
@@ -63,6 +75,216 @@ export default component$(() => {
     h6 {
         overflow-wrap: break-word;
     }
+
+    button { 
+      cursor: pointer;
+    }
   `);
-  return <Slot />;
+
+  useStylesScoped$(`
+    main { 
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      min-height: 100vh;
+      min-height 100dvh;
+    }
+    div { 
+      flex-grow: 1;
+    }
+  `);
+  return (
+    <main>
+      <Header />
+      <div>
+        <Slot />
+      </div>
+      <Footer />
+    </main>
+  );
+});
+
+const Header = component$(() => {
+  useStylesScoped$(`
+    header { 
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+
+    header div { 
+      width: 100%;
+      display: flex;
+      justify-content: flex-end;
+      padding: 12px;
+    }
+    h1 { 
+      text-align: center;
+    }
+    a svg { 
+      height: 1em;
+      width: 1em;
+      margin-bottom: -2px;
+    }
+
+    button { 
+      margin-top: 12px;
+      border-radius: 8px;
+      padding: 4px 12px;
+      border: none;
+    }
+
+    h2 { 
+      font-size: 18px;
+      font-weight: normal;
+    }
+  `);
+
+  const showSearch = useSignal(false);
+  useOnWindow(
+    "keydown",
+    sync$((e: KeyboardEvent) => {
+      if (e.key === "k" && e.ctrlKey) {
+        e.preventDefault();
+      }
+    })
+  );
+  useOnWindow(
+    "keydown",
+    $((e) => {
+      if (e.key === "k" && e.ctrlKey) {
+        showSearch.value = true;
+      }
+    })
+  );
+  const location = useLocation();
+  console.log(location.url.pathname);
+  return (
+    <header>
+      {showSearch.value && <SearchModal showModalSignal={showSearch} />}
+      <div>
+        <a href="https://github.com/KenAKAFrosty/example-movies">
+          <GitHubLogo />
+          Code
+        </a>
+      </div>
+      <h1>
+        {location.url.pathname === "/" ? (
+          "Movies!"
+        ) : (
+          <Link href={"/"}>Movies!</Link>
+        )}
+      </h1>
+      <button
+        onClick$={() => {
+          showSearch.value = true;
+        }}
+      >
+        <h2>Tap here, or press CTRL + K for search</h2>
+      </button>
+    </header>
+  );
+});
+
+const SearchModal = component$(
+  (props: { showModalSignal: Signal<boolean> }) => {
+    useStylesScoped$(`
+    main { 
+      position: fixed;
+      top:0;
+      left: 0;
+      width: 100%;
+      height: 100vh;
+      height: 100dvh;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background-color: rgba(0,0,0,0.1);
+      backdrop-filter: blur(8px);
+      -webkit-backdrop-filter: blur(8px);
+      z-index: 1;
+    }
+    aside { 
+      position: fixed;
+      width: 100%;
+      min-height: 100vh;
+      z-index: 1;
+    }
+    section { 
+      background-color: white;
+      width: 200px;
+      height: 200px;
+      z-index: 2;
+    }
+  `);
+
+    useTask$(() => {
+      if (typeof window === "undefined") return;
+      window.document.body.style.overflow = "hidden";
+      return () => {
+        window.document.body.style.overflow = "auto";
+      };
+    });
+    return (
+      <main>
+        <aside
+          onClick$={() => {
+            props.showModalSignal.value = false;
+          }}
+        />
+        <section></section>
+      </main>
+    );
+  }
+);
+
+const Footer = component$(() => {
+  useStylesScoped$(`
+    footer { 
+      padding: 8px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      gap: 18px;
+      font-size: 12px;
+    }
+    div { 
+      display: flex;
+      align-items: center;
+    }
+    .qwik a { 
+      color: inherit;
+    }
+    .qwik a svg { 
+      height: 2em;
+      margin-bottom: -5px;
+      margin-right: -12px;
+    }
+    .qwik { 
+      font-size: 18px;
+    }
+    .others a { 
+      min-width: 180px;
+      text-align: center;
+    }
+  `);
+  return (
+    <footer>
+      <div class="qwik">
+        <p>Powered by </p>
+        <a href="https://qwik.dev">
+          <QwikLogo />
+        </a>
+      </div>
+      <div class="others">
+        <a href="https://remix-movies.pages.dev/">
+          Ryan's Original Remix Example
+        </a>
+        <a href="https://sveltekit-movies-demo.vercel.app/">
+          Rich's SvelteKit Example
+        </a>
+      </div>
+    </footer>
+  );
 });
